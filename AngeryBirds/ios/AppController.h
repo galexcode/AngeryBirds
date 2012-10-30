08c9eb40afeb778b7f86c0d87cde14ae4650388c
Law Lincoln <lawlincoln@LawtekiMacBook-Pro.local>
2012-10-30 10:57:14 +0800

    Initial Commit

---
+++ AngeryBirds/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  AngeryBirdsAppController.h
+//  AngeryBirds
+//
+//  Created by Law Lincoln on 12-10-30.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ AngeryBirds/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  AngeryBirdsAppController.h
+//  AngeryBirds
+//
+//  Created by Law Lincoln on 12-10-30.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
